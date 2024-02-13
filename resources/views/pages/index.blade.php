81f40bdfad587f61cab5ea30a8e169e13fc7ab39
Martina Ambrosini <martinaambrosini17@gmail.com>
2024-02-13 17:57:45 +0100

    add: added tables with trains infos

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,4 +4,36 @@
 @endsection
 @section('content')
      <h1>Treni: {{count($trains)}}</h1>
+          <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Trains</th>
+                    <th scope="col">Company</th>
+                    <th scope="col">Departure Station</th>
+                    <th scope="col">Arrival Station</th>
+                    <th scope="col">Departure Time</th>
+                    <th scope="col">Arrival Time</th>
+                    <th scope="col">Train Code</th>
+                    <th scope="col">Carriages Number</th>
+                    <th scope="col">In Time</th>
+                    <th scope="col">Deleted</th>
+                </tr>
+            </thead> 
+            <tbody>
+                @foreach ($trains as $train)
+                    <tr>
+                    <th scope="row">{{ $train -> id }}</th>
+                    <td>{{ $train -> company }}</td>
+                    <td>{{ $train -> departure_station }}</td>
+                    <td>{{ $train -> arrival_station }}</td>
+                    <td>{{ $train -> departure_time }}</td>
+                    <td>{{ $train -> arrival_time }}</td>
+                    <td>{{ $train -> train_code }}</td>
+                    <td>{{ $train -> carriages_number }}</td>
+                    <td>{{ $train -> in_time }}</td>
+                    <td>{{ $train -> deleted }}</td>
+                    </tr>
+                @endforeach 
+            </tbody>
+        </table>
 @endsection
